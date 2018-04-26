012e2bb89701b656a6b6b61ce06f7cb6c720556f
Tobias Butler <tobias@change.org>
2018-04-26 12:35:37 -0700

    use traverse on program enter to maintain compatibility with other babel presets

--- src/index.js
+++ src/index.js
@@ -4,43 +4,44 @@ module.exports = ({ types: t }) => {
 
   return {
     visitor: {
-      // Finds uses of _.method and replaces them with _method,
-      // and records which methods were used in a file so that require
-      // statements can be prepended to the file
-      MemberExpression(path) {
-        const {
-          node: { object, property },
-        } = path;
-
-        if (object.name !== '_') return;
-
-        const lodashMethodName = property.name;
-        path.replaceWith(getMethodIdentifier(lodashMethodName));
-        if (lodashMethodsUsed.indexOf(lodashMethodName) < 0) lodashMethodsUsed.push(lodashMethodName);
-      },
-
-      // Remove uses of `const _ = require('lodash')`
-      VariableDeclarator(path) {
-        const {
-          node: {
-            id: { name },
-            init,
-          },
-        } = path;
-
-        if (name !== '_') return;
-        if (!t.isCallExpression(init)) return;
-        if (init.callee.name !== 'require') return;
-        if (!init.arguments) return;
-        if (init.arguments[0].value !== 'lodash') return;
-
-        // if the method hasn't exited, this node is a require of lodash as _
-        path.remove();
-      },
-
       Program: {
-        enter() {
+        enter({ hub: { file } }) {
           lodashMethodsUsed = [];
+          file.path.traverse({
+            // Finds uses of _.method and replaces them with _method,
+            // and records which methods were used in a file so that require
+            // statements can be prepended to the file
+            MemberExpression(path) {
+              const {
+                node: { object, property },
+              } = path;
+
+              if (object.name !== '_') return;
+
+              const lodashMethodName = property.name;
+              path.replaceWith(getMethodIdentifier(lodashMethodName));
+              if (lodashMethodsUsed.indexOf(lodashMethodName) < 0) lodashMethodsUsed.push(lodashMethodName);
+            },
+
+            // Remove uses of `const _ = require('lodash')`
+            VariableDeclarator(path) {
+              const {
+                node: {
+                  id: { name },
+                  init,
+                },
+              } = path;
+
+              if (name !== '_') return;
+              if (!t.isCallExpression(init)) return;
+              if (init.callee.name !== 'require') return;
+              if (!init.arguments) return;
+              if (init.arguments[0].value !== 'lodash') return;
+
+              // if the method hasn't exited, this node is a require of lodash as _
+              path.remove();
+            },
+          });
         },
 
         // add the require statements for individual lodash methods
